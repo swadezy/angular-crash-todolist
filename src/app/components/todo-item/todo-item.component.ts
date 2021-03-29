21d188389730cce2bc0a11b34d80942ab5a1f252
Sean Wade <smwade1115@gmail.com>
2021-03-29 10:49:52 -0500

    trasnferred data into a service

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -4,14 +4,34 @@ import { Todo } from '../../models/Todo';
 @Component({
   selector: 'app-todo-item',
   templateUrl: './todo-item.component.html',
-  styleUrls: ['./todo-item.component.css']
+  styleUrls: ['./todo-item.component.css'],
 })
 export class TodoItemComponent implements OnInit {
   @Input() todo!: Todo;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  // set dynamic classes
+  setClasses() {
+    let classes = {
+      todo: true,
+      // need quotes for classnames with hyphens
+      'is-complete': this.todo.completed,
+    };
+    return classes;
+  }
+
+  onToggle(todo: any) {
+    console.log("toggle");
+    todo.completed = !todo.completed;
+    
+  }
+
+  onDelete(todo: any) {
+    console.log("delete");
+    
   }
 
 }
--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Todo } from '../../models/Todo';
+import { TodoService } from '../../services/todo.service';
 
 @Component({
   selector: 'app-todos',
@@ -10,25 +11,9 @@ export class TodosComponent implements OnInit {
   todos!: Todo[];
 
   // mainly used to import services
-  constructor() {}
+  constructor(private todoService: TodoService) {}
 
   ngOnInit() {
-    this.todos = [
-      {
-        id: 1,
-        title: 'Todo One',
-        completed: false,
-      },
-      {
-        id: 2,
-        title: 'Todo Two',
-        completed: true,
-      },
-      {
-        id: 3,
-        title: 'Todo Three',
-        completed: false,
-      },
-    ];
+    this.todos = this.todoService.getTodos();
   }
 }
