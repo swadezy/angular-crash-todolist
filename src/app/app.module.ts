035cc0918cd8b99b9f934a344fdf8944f4a9e418
Sean Wade <smwade1115@gmail.com>
2021-03-29 11:40:47 -0500

    post working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodosComponent } from './components/todos/todos.component';
 import { TodoItemComponent } from './components/todo-item/todo-item.component';
+import { HeaderComponent } from './components/layout/header/header.component';
+import { AddTodoComponent } from './components/add-todo/add-todo.component';
 
 @NgModule({
   // declarations are where components will go
   // CLI component generation automatically puts them here
-  declarations: [AppComponent, TodosComponent, TodoItemComponent],
+  declarations: [
+    AppComponent,
+    TodosComponent,
+    TodoItemComponent,
+    HeaderComponent,
+    AddTodoComponent,
+  ],
   // using modules like the HTTP or routing module go in imports
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   // providers are services you want to include
   providers: [],
   bootstrap: [AppComponent],
--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -26,4 +26,10 @@ export class TodosComponent implements OnInit {
     // delete on server
     this.todoService.deleteTodo(todo).subscribe();
   }
+
+  addTodo(todo: Todo) {
+    this.todoService.addTodo(todo).subscribe((todo) => {
+      this.todos.push(todo);
+    });
+  }
 }
