e0e8f5f23ec055eb3a049d7bd6657e6b9d3a3709
Sean Wade <smwade1115@gmail.com>
2021-03-29 10:14:47 -0500

    halfway through traversy crash course - todolist rendering todo items

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
+// like importing React
 import { Component } from '@angular/core';
 
 @Component({
+  // creating a component with CLI autofills selector etc
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-crash-todolist';
+  name: string = 'Sean';
+
+  // constructor() {
+  //   console.log(123);
+  //   this.changeName('Mark');
+  // }
+
+  // changeName(name: string): void {
+  //   this.name = name;
+  // }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodosComponent } from './components/todos/todos.component';
+import { TodoItemComponent } from './components/todo-item/todo-item.component';
 
 @NgModule({
+  // declarations are where components will go
+  // CLI component generation automatically puts them here
   declarations: [
-    AppComponent
+    AppComponent,
+    TodosComponent,
+    TodoItemComponent
   ],
+  // using modules like the HTTP or routing module go in imports
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
+  // providers are services you want to include
   providers: [],
   bootstrap: [AppComponent]
 })
